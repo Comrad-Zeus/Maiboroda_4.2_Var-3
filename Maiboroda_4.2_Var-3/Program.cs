fd65e8050feff94dd7cbcd8f1100e84d654d8d55
maybo <mayboroda2045@gmail.com>
2022-12-17 12:55:02 +0200

    Добавьте файлы проекта.

---
+++ Maiboroda_4.2_Var-3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Майборода А.А. Вар 3 - Высокий уровень
+var columnsNumber = int.Parse(Console.ReadLine());
+var rowsNumber = int.Parse(Console.ReadLine());
+
+var array = new int[columnsNumber, rowsNumber];
+var random = new Random();
+Console.WriteLine('\n');
+
+// Заполняем числами и выводим
+for (var columnIndex = 0; columnIndex < columnsNumber; columnIndex++)
+{
+    for (var rowIndex = 0; rowIndex < rowsNumber; rowIndex++)
+    {
+        array[columnIndex, rowIndex] = random.Next(0, 101);
+        Console.Write($"{array[columnIndex, rowIndex]} ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+// Умножаем на 3 и выводим
+for (var columnIndex = 0; columnIndex < columnsNumber; columnIndex++)
+{
+    for (var rowIndex = 0; rowIndex < rowsNumber; rowIndex++)
+    {
+        array[columnIndex, rowIndex] *= 3;
+        Console.Write($"{array[columnIndex, rowIndex]} ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+// Меняем знак и выводим
+for (var columnIndex = 0; columnIndex < columnsNumber; columnIndex++)
+{
+    for (var rowIndex = 0; rowIndex < rowsNumber; rowIndex++)
+    {
+        array[columnIndex, rowIndex] *= -1;
+        Console.Write($"{array[columnIndex, rowIndex]} ");
+    }
+
+    Console.WriteLine();
+}
